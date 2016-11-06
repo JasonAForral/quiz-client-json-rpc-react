60f879714a1939eaf53a7c1088b4c7db58d6803b
JasonAForral <ajsnoface@gmail.com>
2016-11-06 00:59:53 -0700

    catch empty submit

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -9,11 +9,19 @@ class QuestionForm extends Component {
     question: PropTypes.object.isRequired,
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      guessId: -1,
+    }
+  }
+
   handleChange = (guessId) => {
     this.setState({guessId})
   }
 
   handleSubmit = (e) => {
+    e.preventDefault()
     const {
       onSubmit,
       question,
@@ -21,7 +29,9 @@ class QuestionForm extends Component {
     const {
       guessId
     } = this.state
-    e.preventDefault()
+    if (-1 === guessId) {
+      return false
+    }
     onSubmit(question.id, guessId)
     return false
   }
